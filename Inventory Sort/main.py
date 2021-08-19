a4ef6f9362e995d062b9711e2ffaa9d4c7a31a98
Gold_Apple_Vn <ankhanh1566@gmail.com>
2021-08-20 00:53:06 +0700

    file from mincraft

---
+++ Inventory Sort/main.py
@@ -0,0 +1,40 @@
+
+# from os import listdir
+# from os.path import isfile, join
+
+# mypath = r"C:\src\Python\Smart Clicker\Inventory Sort\Bedwars Item"
+# onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+# print(onlyfiles)
+# for x in range(len(onlyfiles)):
+# 	onlyfiles[x] = onlyfiles[x].replace(".png", "")
+
+# print(onlyfiles)
+
+allItem = ['apple_golden', 'arrow', 'bow_standby', 'bucket_water', 
+	'compass', 'diamond', 'diamond_axe', 'diamond_pickaxe', 
+	'diamond_sword', 'emerald', 'ender_pearl', 'fireball', 
+	'gold_axe', 'gold_ingot', 'gold_pickaxe', 'iron_axe', 
+	'iron_ingot', 'iron_pickaxe', 'iron_sword', 'stone_axe', 
+	'stone_pickaxe', 'stone_sword', 'wood_sword', "wood_axe", "wood_pickaxe"]
+
+sword = ["wood_sword", "stone_sword", "iron_sword", "diamond_sword"]
+axes = ["wood_axe", "iron_axe", "diamond_axe", "gold_axe"]
+pickaxe = ["wood_pickaxe", "iron_pickaxe", "diamond_pickaxe", "gold_pickaxe"]
+block = 
+print(onlyfiles)
+class Sort:
+	def __init__(self):
+		table = [0 for x in range(9)]
+		table = [table for y in range(4)]
+		self.table = table
+
+	def sumItem(self, item): 
+		"Sum every item not in a 64 stack in 64"
+		pass
+	def PerferSetUp(self):
+		self.table[0][0] = sword[0]
+		self.table[0][1] = 
+		self.table[0][2] = 
+		self.table[0][3] = 
+		self.table[0][4] = 'apple_golde'
